ac815cded562b832a74e6554a9a1a830400eef5d
VitorFedele <64284863+VitorFedele@users.noreply.github.com>
2022-06-30 14:22:51 -0300

    Add files via upload

---
+++ github/src/app/App.java
@@ -0,0 +1,18 @@
+package app;
+
+import app.pastaaaa.campo;
+import app.pastaaaa.telinhas;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      
+      campo c = new campo();
+      c.addMinas();
+      System.out.println(c);
+      
+      telinhas T = new telinhas();
+
+    }
+
+    
+}
