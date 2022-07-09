7c231c99801dfb309b39a748e86352d7cb2c3dcb
VitorFedele <64284863+VitorFedele@users.noreply.github.com>
2022-07-09 10:25:12 -0300

    Add files via upload

---
+++ src/app/App.java
@@ -0,0 +1,17 @@
+package app;
+
+
+import app.pastaaaa.JTelinhas;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      
+     //System.out.println(c);
+      JTelinhas T = new JTelinhas();
+
+    }
+public static void hardRest(){
+  JTelinhas T = new JTelinhas();
+}
+    
+}
